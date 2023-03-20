099288f35d127283988f3e4b13a6e6a3adff858e
isabelesc <isabelespinosag@gmail.com>
2023-03-20 12:26:20 +0100

    a ver que hace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador.module';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeroeComponent,
+    ListadoComponent
+    ],
   imports: [
     BrowserModule,
+    ContadorModule,
     AppRoutingModule
   ],
   providers: [],
